5ae299058a363b58c23782baf5b3098b2c613886
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-01-16 16:22:35 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './conponents/Header';
+import Slider from './conponents/Slider';
+import Detail from './conponents/Detail';
+import Products from './conponents/Products';
+import Find from './conponents/Find';
+import Client from './conponents/Client';
+import Sign from './conponents/Sign';
+import Info from './conponents/Info';
+import Footer from './conponents/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div className="hero_area">
+          {/* header section strats */}
+            <Header></Header>
+          {/* end header section */}
+          {/* slider section */}
+            <Slider></Slider>
+          {/* end slider section */}
+        </div>
+        {/* detail section */}
+          <Detail></Detail>
+        {/* end detail section */}
+        {/* products section */}
+          <Products></Products>
+        {/* end products section */}
+        {/* find section */}
+          <Find></Find>
+        {/* end find section */}
+        {/* client section */}
+        <Client></Client>
+        {/* end client section */}
+        {/* sign section */}
+        <Sign></Sign>
+        {/* end sign section */}
+        {/* info section */}
+        <Info></Info>
+        {/* end info section */}
+        {/* footer section */}
+        <Footer></Footer>
+        {/* footer section */}
+      </>
     </div>
   );
 }
---
+++ src/conponents/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+        <section className="container-fluid footer_section">
+        <p>
+          Copyright © All Rights Reserved By
+          <a href="https://html.design/">Free Html Templates</a>
+        </p>
+      </section>
+    )
+  }
+}
